b8c7f4c9cba599e15c0b28119b8dd11ba4199d39
0718-lari <larisilvacys@gmail.com>
2026-04-08 19:27:02 -0300

    Subindo Atividade

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const server = express();
+
+server.use((req, res, next) => {
+    console.log('Acesso Permitido');
+    next();
+});
+
+server.use((req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+});
+
+
+server.get('/', (req, res) => {
+    console.log('Tempo da requisição:', req.requestTime);
+    res.sendFile('home.html', { root: __dirname });
+});
+
+
+server.get('/gabriela', (req, res) => {
+    res.sendFile('gabriela.html', { root: __dirname });
+});
+
+server.get('/larissa', (req, res) => {
+    res.sendFile('larissa.html', { root: __dirname });
+});
+
+
+server.use((req, res) => {
+    res.status(404).sendFile('erro.html', { root: __dirname });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando em http://localhost:3000');
+});
